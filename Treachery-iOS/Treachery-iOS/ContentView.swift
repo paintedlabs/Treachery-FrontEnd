c7dafc652921f2423e5600b0fb56e8cf8260686a
Luke Solomon <solomora@gmail.com>
2024-09-10 12:17:45 -0400

    Initial commit - setting up Repo

---
+++ Treachery-iOS/Treachery-iOS/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Treachery-iOS
+//
+//  Created by Luke Solomon on 9/10/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+
+    var body: some View {
+      Text("Hello World")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Treachery-iOS/Treachery-iOSTests/Treachery_iOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  Treachery_iOSTests.swift
+//  Treachery-iOSTests
+//
+//  Created by Luke Solomon on 9/10/24.
+//
+
+import Testing
+
+struct Treachery_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
